74123a3624b97be39b997bff8c4e0bf39e2fccdb
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2024-06-26 14:56:22 +0800

    Initialize client

---
+++ client/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#164863",
+                secondary: "#427D9D",
+                tertiary: "#9BBEC8",
+                quaternary: "#DDF2FD",
+                light: "gray",
+                success: "#2DD36F",
+                warning: "#FFC409",
+                failure: "#EB445A",
+            },
+        },
+    },
+    plugins: [],
+};
