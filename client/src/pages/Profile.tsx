18ddd1e8c566664db4eeb5ce3e7decba006ffb0f
Edward Vincent Cuevas <edcuevas@fullscale.ph>
2024-07-11 19:10:05 +0800

    Create profile page

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,7 +1,22 @@
+import { useUser } from "@store/slices/user";
 import { FC } from "react";
 
 const Profile: FC = () => {
-    return <div>Profile</div>;
+    const { user } = useUser();
+
+    return (
+        <div className="flex h-full">
+            <div className="w-96 h-full flex flex-col items-center gap-4">
+                <div className="w-32 h-32 rounded-full bg-gray-400"></div>
+                <div className="flex flex-col items-center">
+                    <p>
+                        {user?.first_name} {user?.last_name}
+                    </p>
+                    <p className="text-sm italic text-gray-500">@{user?.username}</p>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Profile;
