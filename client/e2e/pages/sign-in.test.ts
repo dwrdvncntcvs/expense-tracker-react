2e929f3c40c1249fe3141ba310852d1efca4db38
Edward Vincent Cuevas <edcuevas@fullscale.ph>
2024-07-23 16:45:05 +0800

    Group test cases per pages

--- client/e2e/pages/sign-in.test.ts
+++ client/e2e/pages/sign-in.test.ts
@@ -1,65 +1,76 @@
 import test, { expect } from "../fixtures";
 import { EMAIL, PASSWORD } from "../variables/auth";
 
-test.beforeEach(async ({ signInPage }) => {
-    signInPage.goto();
-});
+test.describe("Sign In Page", () => {
+    test.beforeEach(async ({ signInPage }) => {
+        signInPage.goto();
+    });
 
-test("Successful - Submitting valid fields", async ({ page, signInPage }) => {
-    await signInPage.inputField("email", EMAIL);
-    await signInPage.inputField("password", PASSWORD);
-    await signInPage.signInButton.click();
+    test("Successful - Submitting valid fields", async ({
+        page,
+        signInPage,
+    }) => {
+        await signInPage.inputField("email", EMAIL);
+        await signInPage.inputField("password", PASSWORD);
+        await signInPage.signInButton.click();
 
-    await page.waitForURL("/");
+        await page.waitForURL("/");
 
-    await expect(signInPage.signInButton).not.toBeVisible();
-});
+        await expect(signInPage.signInButton).not.toBeVisible();
+    });
 
-test("Navigation - Go to sign up when 'Doesn't have an account?' was clicked", async ({
-    signInPage,
-    page,
-}) => {
-    await signInPage.noAccount.click();
+    test("Navigation - Go to sign up when 'Doesn't have an account?' was clicked", async ({
+        signInPage,
+        page,
+    }) => {
+        await signInPage.noAccount.click();
 
-    await page.waitForURL("/sign-up");
+        await page.waitForURL("/sign-up");
 
-    await expect(page.getByRole("button", { name: "Sign Up" })).toBeVisible();
-});
+        await expect(
+            page.getByRole("button", { name: "Sign Up" })
+        ).toBeVisible();
+    });
 
-test("Failed - Submitting empty fields", async ({ signInPage }) => {
-    await signInPage.inputField("email");
-    await signInPage.inputField("password");
+    test("Failed - Submitting empty fields", async ({ signInPage }) => {
+        await signInPage.inputField("email");
+        await signInPage.inputField("password");
 
-    await signInPage.signInButton.click();
+        await signInPage.signInButton.click();
 
-    await expect(signInPage.fieldError("Email is required")).toBeVisible();
-    await expect(signInPage.fieldError("Password is required")).toBeVisible();
-});
+        await expect(signInPage.fieldError("Email is required")).toBeVisible();
+        await expect(
+            signInPage.fieldError("Password is required")
+        ).toBeVisible();
+    });
 
-test("Failed - Signing in with incorrect credentials", async ({
-    page,
-    signInPage,
-}) => {
-    await signInPage.inputField("email", EMAIL);
-    await signInPage.inputField("password", "incorrect-pass");
+    test("Failed - Signing in with incorrect credentials", async ({
+        page,
+        signInPage,
+    }) => {
+        await signInPage.inputField("email", EMAIL);
+        await signInPage.inputField("password", "incorrect-pass");
 
-    await signInPage.signInButton.click();
+        await signInPage.signInButton.click();
 
-    const toast = page.getByText("Invalid Username or Password");
+        const toast = page.getByText("Invalid Username or Password");
 
-    await expect(toast).toBeVisible();
+        await expect(toast).toBeVisible();
 
-    await page.waitForTimeout(5000);
+        await page.waitForTimeout(5000);
 
-    await expect(toast).not.toBeVisible();
-});
+        await expect(toast).not.toBeVisible();
+    });
 
-test("Failed - Signing in with invalid email format", async ({
-    signInPage,
-}) => {
-    await signInPage.inputField("email", "sample.com");
-    await signInPage.inputField("password", PASSWORD);
-    await signInPage.signInButton.click();
+    test("Failed - Signing in with invalid email format", async ({
+        signInPage,
+    }) => {
+        await signInPage.inputField("email", "sample.com");
+        await signInPage.inputField("password", PASSWORD);
+        await signInPage.signInButton.click();
 
-    await expect(signInPage.fieldError("Invalid email format")).toBeVisible();
+        await expect(
+            signInPage.fieldError("Invalid email format")
+        ).toBeVisible();
+    });
 });
--- client/e2e/pages/sign-out.test.ts
+++ client/e2e/pages/sign-out.test.ts
@@ -1,22 +1,26 @@
 import test, { expect } from "../fixtures";
 import { EMAIL, PASSWORD } from "../variables/auth";
 
-test.beforeEach(async ({ auth }) => {
-    await auth.authenticate(EMAIL, PASSWORD);
-});
+test.describe("Sign Out", () => {
+    test.beforeEach(async ({ auth }) => {
+        await auth.authenticate(EMAIL, PASSWORD);
+    });
 
-test("Success - Sign out", async ({ page }) => {
-    await page.waitForURL("/");
+    test("Success - Sign out", async ({ page }) => {
+        await page.waitForURL("/");
 
-    const logoutBtn = page.locator("#logout");
+        const logoutBtn = page.locator("#logout");
 
-    await expect(logoutBtn).toBeVisible();
+        await expect(logoutBtn).toBeVisible();
 
-    await logoutBtn.click();
+        await logoutBtn.click();
 
-    await page.waitForURL("/sign-in");
+        await page.waitForURL("/sign-in");
 
-    await expect(logoutBtn).not.toBeVisible();
+        await expect(logoutBtn).not.toBeVisible();
 
-    await expect(page.getByRole("button", { name: "Sign In" })).toBeVisible();
+        await expect(
+            page.getByRole("button", { name: "Sign In" })
+        ).toBeVisible();
+    });
 });
