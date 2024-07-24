98983e8d79ac911597223a25ab50539caceab3b1
Edward Vincent Cuevas <edcuevas@fullscale.ph>
2024-07-24 21:07:32 +0800

    Add additional guard test

--- e2e/tests/pages/guards.test.ts
+++ e2e/tests/pages/guards.test.ts
@@ -1,7 +1,7 @@
 import test, { expect } from "../fixtures";
 import { EMAILS, PASSWORD } from "../variables/auth";
 
-test.describe("Page Guards", () => {
+test.describe("Page Guards - Public Page", () => {
     test("Navigate back - Should be back to sign in page when trying to access home page when not authenticated", async ({
         signInPage,
         page,
@@ -23,6 +23,36 @@ test.describe("Page Guards", () => {
 
         await page.goto("/sign-in");
 
+        await page.waitForURL("/");
+
+        await expect(page.locator("#logout")).toBeVisible();
+    });
+});
+
+test.describe("Page Guards - Private Page", async () => {
+    test.beforeEach(async ({ auth, page }) => {
+        await auth.authenticate(EMAILS[0], PASSWORD);
+
+        await page.waitForURL("/");
+    });
+
+    test("Navigate back - should be back to home page when navigate from home page to sign in page", async ({
+        page,
+    }) => {
+        await page.goto("/sign-in");
+
+        await page.waitForURL("/sign-in");
+        await page.waitForURL("/");
+
+        await expect(page.locator("#logout")).toBeVisible();
+    });
+
+    test("Navigate back - should be back to home page when navigate from home page to sgn up page", async ({
+        page,
+    }) => {
+        await page.goto("/sign-up");
+
+        await page.waitForURL("/sign-up");
         await page.waitForURL("/");
 
         await expect(page.locator("#logout")).toBeVisible();
